4e5c63db90878ea744ce31f86120975c31ee5955
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-26 01:37:00 +0600

    update folder structure

---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const userSignupSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+  phone: z.string().length(10, { message: "Phone must be 10 digits" }),
+  address: z
+    .string()
+    .min(5, { message: "Address must be at least 5 characters long" }),
+  role: z.enum(["admin", "user"], {
+    message: "Role must be either 'admin' or 'user'",
+  }),
+});
+
+export const AuthValidations = {
+  userSignupSchema,
+};
