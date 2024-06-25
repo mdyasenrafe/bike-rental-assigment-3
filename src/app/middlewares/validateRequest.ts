f8813add1fa02db2934f9dec6dbdfb971dc57f30
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-26 02:11:24 +0600

    removed console

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -4,8 +4,7 @@ import { AnyZodObject } from "zod";
 export const validateRequest = (schema: AnyZodObject) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const res = await schema.parseAsync(req.body);
-      console.log("next =>", res);
+      await schema.parseAsync(req.body);
       next();
     } catch (err) {
       next(err);
