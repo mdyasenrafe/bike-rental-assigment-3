3edb6685528dafcdb17a984f490c4725e9f0b16b
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-26 01:49:08 +0600

    auth controller updated

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-const register = () => {};
+import { Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+
+const register = () =>
+  catchAsync(async (req: Request, res: Response) => {
+    sendResponse(res, {
+      data: null,
+      message: "User registered successfully",
+    });
+  });
 
 export const AuthControllers = {
   register,
