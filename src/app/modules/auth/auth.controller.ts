7380bb0db2b04da6c8e6a287a8b22f288eb61ee7
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-26 01:39:34 +0600

    auth router file setup done

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,5 @@
+const register = () => {};
+
+export const AuthControllers = {
+  register,
+};
---
+++ src/app/modules/auth/auth.route.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { AuthValidations } from "./auth.validation";
+import { AuthControllers } from "./auth.controller";
+
+const router = express.Router();
+
+router.post(
+  "/signup",
+  validateRequest(AuthValidations.userSignupSchema),
+  AuthControllers.register
+);
