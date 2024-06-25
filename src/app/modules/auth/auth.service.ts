6fdef55f0d640d4e9ae255298a7a0b668661028c
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-26 02:29:22 +0600

    app error added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,3 +1,5 @@
+import httpStatus from "http-status";
+import { AppError } from "../../errors/appError";
 import { TUser } from "../user/user.interface";
 import { UserModel } from "../user/user.model";
 import bcrypt from "bcrypt";
@@ -10,11 +12,11 @@ const createUserIntoDB = async (payload: TUser) => {
 const signinUser = async (email: string, password: string) => {
   const user = await UserModel.findOne({ email });
   if (!user) {
-    throw new Error("User not found");
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
   }
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
-    throw new Error("Invalid credentials");
+    throw new AppError(httpStatus.UNAUTHORIZED, "Invalid credentials");
   }
   return { data: user };
 };
