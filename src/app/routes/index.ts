164e7a4d7b21d5f68b45c5a8925f8aeb9184325f
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-26 15:09:35 +0600

    added users routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthRoutes } from "../modules/auth/auth.route";
+import { userRoutes } from "../modules/user/user.route";
 
 const router = Router();
 
@@ -7,6 +8,10 @@ const modulesRoutes = [
   {
     path: "/auth",
     route: AuthRoutes,
+  },
+  {
+    path: "/users",
+    route: userRoutes,
   },
 ];
 
