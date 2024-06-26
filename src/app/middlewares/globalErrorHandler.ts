b5b41618568f3a77ae9428f71c81c3dbd88350ef
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-27 02:12:01 +0600

    cast and duplicate error formet done

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,8 @@ import { TErrorSources } from "../types/error";
 import { ZodError } from "zod";
 import { handleZodError } from "../errors/handleZodError";
 import { handleValidationError } from "../errors/handleValidationError";
+import { handleDuplicateError } from "./handleDuplicateError";
+import { handleCastError } from "../errors/handleCastError";
 
 export const globalErrorHandler = (
   err: any,
@@ -25,6 +27,16 @@ export const globalErrorHandler = (
     errorSources = simplifiedError?.errorSources;
   } else if (err?.name === "ValidationError") {
     const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
