be3a689cabf5c060766ffd8ce7ac00a7a043d130
Md yasen rafe <mdyasenrafe@gmail.com>
2024-06-26 22:48:34 +0600

    hassed password added and role can't be changed

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -4,6 +4,7 @@ import { TUser } from "./user.interface";
 import { JwtPayload } from "jsonwebtoken";
 import { AppError } from "../../errors/appError";
 import httpStatus from "http-status";
+import bcrypt from "bcrypt";
 
 const getUserFromDB = async (id: Types.ObjectId) => {
   const result = await UserModel.findById(id);
@@ -15,11 +16,14 @@ const updateUserIntoDB = async (
   payload: Partial<TUser>
 ) => {
   const { userId, role } = currentUser;
-  if ("role" in payload && role !== "admin") {
+  if ("role" in payload) {
     throw new AppError(
-      httpStatus.UNAUTHORIZED,
-      "Unauthorized role change attempt: Only users with the admin role can change user roles. Please remove the role field from the request body if you wish to proceed."
+      httpStatus.BAD_REQUEST,
+      "Role cannot be updated. Please remove the role field from the request body if you wish to proceed."
     );
+  }
+  if (payload.password) {
+    payload.password = await bcrypt.hash(payload.password, 12);
   }
   const result = await UserModel.findByIdAndUpdate(userId, payload, {
     new: true,
