639734307213278199182e3f53fb38ef7ab36fc2
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-05 20:08:56 +0600

    webhooks bug resolved

--- src/app/modules/payment/paymentServices.ts
+++ src/app/modules/payment/paymentServices.ts
@@ -2,8 +2,12 @@ import { stripe } from "../../config";
 
 export const verifyStripeWebhookSignature = (req: any) => {
   const sig = req.headers["stripe-signature"] as string;
+  console.log(
+    "process.env.STRIPE_WEBHOOK_SECRET",
+    process.env.STRIPE_WEBHOOK_SECRET
+  );
   const event = stripe.webhooks.constructEvent(
-    req.body,
+    req.rawBody,
     sig,
     process.env.STRIPE_WEBHOOK_SECRET!
   );
